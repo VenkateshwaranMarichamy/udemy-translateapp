d97629c8f07959ad25756e4bb3bc1ed9b04ac351
venkatesh <venkateshwaranmarichamy@gmail.com>
2022-07-02 23:49:25 +0530

    commit after the inital draft

---
+++ src/components/translate.js
@@ -0,0 +1,69 @@
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function Translate({language, text}){
+const [translated] = useTranslation(text,language);
+
+    return(
+        <div className='translate'>
+            <label className='label'>Output</label>
+            <h1 className='title' >{translated.replace("&#39;", "'")}</h1>
+        </div>
+    );
+
+    const useTranslation = (language, text) =>{
+        const [translated, setTranslated] = useState("");
+
+        useEffect(() => {
+                if(!text){
+                    return;
+                }
+
+            const cancelToken = Axios.CancelToken.source();
+            doTranslation(text, language,cancelToken,setTranslated);
+
+        return () => {
+            try{
+                cancelToken.cancel;
+            }catch(err){}
+        };
+    }, [text,language]);
+    retur [translated];
+
+    };
+
+    const debounce= (fn) => {
+        let id = null;
+
+        return (...args) => {
+            if(id){
+                clearTimeout(id);
+            }
+
+            id = setTimeout(() => {
+                fn(...args);
+                id=null;
+            },300);
+        };
+    };
+
+    const doTranslation = debounce(
+        async (input, languageCode, cancelToken, setTranslated) => {
+            try{
+                const {data} = await Axios.post(
+                    "https://translation.googleapis.com/language/translate/v2?key=AIzaSyCf0Xy0OnhxlduyEt3K8zP-sOuu-l_u6uA",
+                    {
+                        q: input,
+                        target: languageCode
+                    }, {cancelToken: cancelToken.token} 
+                );
+
+                callback(data.data.translations[0].translatedText);
+            }catch(err){
+                callback("");
+            }
+        }
+    )
+}
+
+export default translate;
